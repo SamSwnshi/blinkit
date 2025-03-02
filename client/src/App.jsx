40bd24911f6a0a776f4cc5f7defd89c8298b2daf
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-03-02 23:45:07 +0530

    new images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main >
+      <main className="min-h-[80vh]">
         <Outlet />
       </main>
       <Footer/>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/logo.png"
 
 const Header = () => {
   return (
-    <header className='h-24 shadow-md'>
-      <h1>Header</h1>
+    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
+      <div className='container mx-auto flex items-center px-2 justify-between'>
+        <div className='h-full'>
+          <div className='h-full flex  justify-center'>
+            <img src={logo} width={170} height={60} alt="logo"  className='hidden lg:block' />
+            <img src={logo} width={170} height={60} alt="logo" className='lg:hidden'/>
+          </div>
+        </div>
+        <div className='hidden lg:block'>
+          searcj
+        </div>
+        <div>
+          login
+        </div>
+      </div>
     </header>
   )
 }
