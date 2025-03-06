0c35a715b910d936109e55c2b8a08923415588c4
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-03-06 11:22:58 +0530

    product routes completed

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,16 +2,18 @@ import { Router } from "express";
 import auth from "../middleware/auth.js";
 import admin from "../middleware/admin.js";
 
+import { createProduct, getProduct, getProductCategory, getProductCategorySubCategory, deleteProduct, getProductDetails, getProductSearch, updateProduct } from "../controllers/product.controllers.js";
+
 const router = Router();
 
 router.post("/create", auth, admin, createProduct);
 router.get("/get", getProduct);
 router.post("/get-product-by-category", getProductCategory);
-router.post("/get-product-by-category-and-subcategory", getProductCategorySub);
+router.post("/get-product-by-category-and-subcategory", getProductCategorySubCategory);
 router.post("/get-product-details,", getProductDetails);
 
-router.put("/update-product", auth, admin, getProductUpdate);
-router.delete("/delete-product", auth, admin, getProductDelete);
+router.put("/update-product", auth, admin, updateProduct);
+router.delete("/delete-product", auth, admin, deleteProduct);
 router.post("/search-product", auth, admin, getProductSearch);
 
 export default router;
