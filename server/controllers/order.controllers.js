f8763bd57a096558b8f132a9075fefb1f73fa549
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-03-15 13:24:06 +0530

    cash on delivery routes

--- server/controllers/order.controllers.js
+++ server/controllers/order.controllers.js
@@ -2,22 +2,55 @@ import CartProductModel from "../models/cartproduct.models.js";
 import OrderModel from "../models/order.models.js";
 import UserModel from "../models/user.models.js";
 
+export const cashOnDelivery = async (req, res) => {
+  try {
+    const userId = req.userId; // auth middleware
+    const { list_items, totalAmt, addressId, subTotalAmt } = req.body;
 
-export const CashOnDelivery = async(req ,res) =>{
-    
-}
-export const priceWithDiscount = async(req ,res) =>{
+    const payload = list_items.map((el) => {
+      return {
+        userId: userId,
+        orderId: `ORD-${new mongoose.Types.ObjectId()}`,
+        productId: el.productId._id,
+        product_details: {
+          name: el.productId.name,
+          image: el.productId.image,
+        },
+        paymentId: "",
+        payment_status: "CASH ON DELIVERY",
+        delivery_address: addressId,
+        subTotalAmt: subTotalAmt,
+        totalAmt: totalAmt,
+      };
+    });
 
-}
-export const payment = async(req ,res) =>{
+    const generatedOrder = await OrderModel.insertMany(payload);
 
-}
-export const getOrderProduct = async(req ,res) =>{
+    ///remove from the cart
+    const removeCartItems = await CartProductModel.deleteMany({
+      userId: userId,
+    });
+    const updateInUser = await UserModel.updateOne(
+      { _id: userId },
+      { shopping_cart: [] }
+    );
 
-}
-export const webhook = async(req ,res) =>{
-
-}
-export const getOrderDetails = async(req ,res) =>{
-
-}
+    return res.json({
+      message: "Order successfully",
+      error: false,
+      success: true,
+      data: generatedOrder,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+export const priceWithDiscount = async (req, res) => {};
+export const payment = async (req, res) => {};
+export const getOrderProduct = async (req, res) => {};
+export const webhook = async (req, res) => {};
+export const getOrderDetails = async (req, res) => {};
--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
+import { cashOnDelivery } from "../controllers/order.controllers.js";
 
 const router = Router();
 
-router.post("/cash-on-delivery",auth)
+router.post("/cash-on-delivery",auth,cashOnDelivery)
 router.post("/checkout",auth)
 router.post("/webhook",auth)
 router.get("/order-list",auth)
