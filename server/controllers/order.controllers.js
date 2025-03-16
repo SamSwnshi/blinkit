ef70d91e596f77131c44d5ecfb512ce1fc22b432
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-03-16 13:14:02 +0530

    Updated GlobalProvider

--- server/controllers/order.controllers.js
+++ server/controllers/order.controllers.js
@@ -1,15 +1,13 @@
+import Stripe from "../db/stripe.js";
 import CartProductModel from "../models/cartproduct.models.js";
 import OrderModel from "../models/order.models.js";
 import UserModel from "../models/user.models.js";
 import mongoose from "mongoose";
 
-
 export const cashOnDelivery = async (req, res) => {
   try {
     const userId = req.user.id; // auth middleware
     const { list_items, totalAmt, addressId, subTotalAmt } = req.body;
-  
-    
 
     const payload = list_items.map((el) => {
       return {
@@ -54,8 +52,122 @@ export const cashOnDelivery = async (req, res) => {
     });
   }
 };
-export const priceWithDiscount = async (req, res) => {};
-export const payment = async (req, res) => {};
-export const getOrderProduct = async (req, res) => {};
+export const pricewithDiscount = (price, dis = 1) => {
+  const discountAmout = Math.ceil((Number(price) * Number(dis)) / 100);
+  const actualPrice = Number(price) - Number(discountAmout);
+  return actualPrice;
+};
+export const payment = async (req, res) => {
+  try {
+    const userId = req.user.id; // auth middleware
+    const { list_items, totalAmt, addressId, subTotalAmt } = req.body;
+
+    console.log("Received Payment Request:", req.body);
+
+    const user = await UserModel.findById(userId);
+
+    const line_items = list_items.map((item) => {
+      return {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: item.productId.name,
+            images: item.productId.image,
+            metadata: {
+              productId: item.productId._id,
+            },
+          },
+          unit_amount:
+            pricewithDiscount(item.productId.price, item.productId.discount) *
+            100,
+        },
+        adjustable_quantity: {
+          enabled: true,
+          minimum: 1,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const params = {
+      submit_type: "pay",
+      mode: "payment",
+      payment_method_types: ["card"],
+      customer_email: user.email,
+      metadata: {
+        userId: userId,
+        addressId: addressId,
+      },
+      line_items: line_items,
+      success_url: `${process.env.FRONTEND_URL}/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/cancel`,
+    };
+
+    console.log("Stripe Checkout Params:", JSON.stringify(params, null, 2));
+
+    const session = await Stripe.checkout.sessions.create(params);
+
+    return res.status(200).json(session);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
 export const webhook = async (req, res) => {};
-export const getOrderDetails = async (req, res) => {};
+export const getOrderProduct = async ({
+  lineItems,
+  userId,
+  addressId,
+  paymentId,
+  payment_status,
+}) => {
+  const productList = [];
+
+  if (lineItems?.data?.length) {
+    for (const item of lineItems.data) {
+      const product = await Stripe.products.retrieve(item.price.product);
+
+      const paylod = {
+        userId: userId,
+        orderId: `ORD-${new mongoose.Types.ObjectId()}`,
+        productId: product.metadata.productId,
+        product_details: {
+          name: product.name,
+          image: product.images,
+        },
+        paymentId: paymentId,
+        payment_status: payment_status,
+        delivery_address: addressId,
+        subTotalAmt: Number(item.amount_total / 100),
+        totalAmt: Number(item.amount_total / 100),
+      };
+
+      productList.push(paylod);
+    }
+  }
+
+  return productList;
+};
+export const getOrderDetails = async (req, res) => {
+  try {
+    const userId = req.user.id // order id
+
+    const orderlist = await OrderModel.find({ userId : userId }).sort({ createdAt : -1 }).populate('delivery_address')
+
+    return res.json({
+        message : "order list",
+        data : orderlist,
+        error : false,
+        success : true
+    })
+} catch (error) {
+    return res.status(500).json({
+        message : error.message || error,
+        error : true,
+        success : false
+    })
+}
+};
--- server/routes/order.routes.js
+++ server/routes/order.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import auth from "../middleware/auth.js";
-import { cashOnDelivery } from "../controllers/order.controllers.js";
+import { cashOnDelivery, payment,getOrderDetails } from "../controllers/order.controllers.js";
 
 const router = Router();
 
 router.post("/cash-on-delivery",auth,cashOnDelivery)
-router.post("/checkout",auth)
+router.post("/checkout",auth,payment)
 router.post("/webhook",auth)
-router.get("/order-list",auth)
+router.get("/order-list",auth,getOrderDetails)
 export default router;
